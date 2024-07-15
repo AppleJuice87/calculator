4bc5ea7e5806c87f464d4489e14fd0227f490948
AppleJuice87 <mujin0305@naver.com>
2024-07-15 15:31:15 +0900

    #2021-git/1 스켈레톤 작성.

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+
+int main() {
+	// test caces
+}
+
+// 뼈대 코드
+
+int add(int a, int b) {
+    //TODO: add
+}
+
+int subtract(int a, int b) {
+    //TODO: subtract
+}
+
+int multiply(int a, int b) {
+    //TODO: multiply
+}
+
+float divide(int a, int b) {
+	//TODO: divide
+}
