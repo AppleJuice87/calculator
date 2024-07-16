9bd93c144a996463b964be0054042e87ca31412e
AppleJuice87 <mujin0305@naver.com>
2024-07-16 09:31:09 +0900

    1

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int add(int num1, int num2);
 int subtract(int num1, int num2);
 int multiply(int num1, int num2);
 float divide(int num1, int num2);
-
+//1
 int main() {
 	// 충돌 유도를 위해 아래 default 문장 뒤에 아무 단어나 추가해 주세요.
 	printf("여기 default 문장이 있습니다. 이 앞 에러있다!!!!!");
